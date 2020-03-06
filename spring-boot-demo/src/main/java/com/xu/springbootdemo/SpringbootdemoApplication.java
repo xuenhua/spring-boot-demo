0d49e386a68866e498dc9f5d78ec8224ad578bf5
xuenhua <xuenhua@aisino.com>
2020-03-06 14:48:54 +0800

    cron

--- spring-boot-demo/src/main/java/com/xu/springbootdemo/SpringbootdemoApplication.java
+++ spring-boot-demo/src/main/java/com/xu/springbootdemo/SpringbootdemoApplication.java
@@ -2,8 +2,10 @@ package com.xu.springbootdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling //定时任务
 public class SpringbootdemoApplication {
 
 	public static void main(String[] args) {
